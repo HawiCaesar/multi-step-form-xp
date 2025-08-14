1cc397264d87f72a5582adb89bc6c78eff09dea6
HawiCaesar <brianhawi92@gmail.com>
2025-08-14 17:14:22 +0300

    fix(): import thank you image directly

--- src/components/ThankYou/index.tsx
+++ src/components/ThankYou/index.tsx
@@ -1,7 +1,9 @@
+import iconThankYou from '../../assets/images/icon-thank-you.svg';
+
 export const ThankYou = () => {
   return (
     <div className='flex flex-col gap-4'>
-      <img src={'/src/assets/images/icon-thank-you.svg'} alt='thank you' className="mx-auto"/>
+      <img src={iconThankYou} alt='thank you' className="mx-auto"/>
       <h1 className='text-2xl font-bold text-[#04295a] text-center'>Thank you!</h1>
       <p className='text-[#a2a3a9] text-center px-4 pb-6'>
         Thanks for confirming your subscription! We hope you have fun using our
